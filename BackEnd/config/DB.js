02ed60fe4e5fbcd8cf6b897281dc25758482d422
hassanq707 <qadrihassan25@gmail.com>
2025-06-13 03:21:31 +0500

    connect_db

--- BackEnd/config/DB.js
+++ BackEnd/config/DB.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(
-        'mongodb+srv://hassanq_706:JDc2ktmDOPsT7G7b@cluster0.za2s4fw.mongodb.net/Food_App'
-        );
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("DB Connected");
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
     }
-    };
+};
 
 module.exports = {
     connectDB
-}
+};
