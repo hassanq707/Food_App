42f54d3221c7d59edf33403a115f7437abd9d441
hassanq707 <qadrihassan25@gmail.com>
2025-06-13 04:05:42 +0500

    mongo_correct

--- BackEnd/config/DB.js
+++ BackEnd/config/DB.js
@@ -2,13 +2,29 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, 
+            socketTimeoutMS: 45000, 
+        });
         console.log("DB Connected");
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
+        process.exit(1);
     }
 };
 
-module.exports = {
-    connectDB
-};
+mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose disconnected");
+});
+
+module.exports = { connectDB };
